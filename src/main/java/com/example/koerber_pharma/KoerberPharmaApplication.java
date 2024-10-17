38b3f55af5c7ef02260cab9b9ff55db7fba101b6
Miguel Lopes <miguellopes@MacBookPro.Home>
2024-10-17 19:36:15 +0100

    skeleton project

---
+++ src/main/java/com/example/koerber_pharma/KoerberPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.koerber_pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoerberPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoerberPharmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/koerber_pharma/KoerberPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koerber_pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoerberPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
